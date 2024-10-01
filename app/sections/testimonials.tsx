f177b588c058ae7a26b226fc88774657a7e6beea
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2024-10-01 19:39:35 +0200

    Style testimonials

--- app/sections/testimonials.tsx
+++ app/sections/testimonials.tsx
@@ -4,34 +4,96 @@ import Testimonial from "@/components/ui/testimonial";
 export default function Testimonials() {
   return (
     <Section>
-      <div className="max-w-container text-center flex flex-col items-center gap-4 sm:gap-8 mx-auto">
-        <h2 className="text-2xl sm:text-5xl font-semibold">
-          Loved by designers and developers across the planet
-        </h2>
-        <p className="text-sm sm:text-xl font-medium text-muted-foreground max-w-[600px]">
-          Here&apos;s what people are saying about Jupiter
-        </p>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          <Testimonial
-            name="Ryan Dick"
-            username="ryandick"
-            text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
-          />
-          <Testimonial
-            name="Ryan Dick"
-            username="ryandick"
-            text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
-          />
-          <Testimonial
-            name="Ryan Dick"
-            username="ryandick"
-            text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
-          />
-          <Testimonial
-            name="Ryan Dick"
-            username="ryandick"
-            text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
-          />
+      <div className="max-w-container text-center flex flex-col items-center gap-4 sm:gap-16 mx-auto overflow-hidden">
+        <div className="flex flex-col gap-4 sm:gap-8 items-center">
+          <h2 className="text-2xl sm:text-5xl font-semibold">
+            Loved by designers and developers across the planet
+          </h2>
+          <p className="text-sm sm:text-xl font-medium text-muted-foreground max-w-[600px]">
+            Here&apos;s what people are saying about Jupiter
+          </p>
+        </div>
+        <div
+          className="overflow-hidden w-full"
+          style={{
+            maskImage:
+              "linear-gradient(to right, transparent 0%, black 25%, black 75%, transparent 100%)",
+          }}
+        >
+          <div className="flex flex-col gap-4 -mx-48 items-center">
+            <div className="flex gap-4 items-center justify-center">
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+            </div>
+            <div className="flex gap-4 items-center justify-center">
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+            </div>
+            <div className="flex gap-4 items-center justify-center">
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+              <Testimonial
+                className="w-[420px]"
+                name="Ryan Dick"
+                username="ryandick"
+                text="Jupiter is the best template I used so far. I really like it and it already help my business make a lot of money."
+              />
+            </div>
+          </div>
         </div>
       </div>
     </Section>
--- components/ui/testimonial.tsx
+++ components/ui/testimonial.tsx
@@ -1,25 +1,37 @@
 import * as React from "react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
 
 export interface TestimonialProps {
   name: string;
   username: string;
   text: string;
+  className?: string;
 }
 
 export default function Testimonial({
   name,
   username,
   text,
+  className,
 }: TestimonialProps) {
   return (
-    <div className="p-6 border rounded-lg flex flex-col items-center text-center">
-      <Avatar className="mb-4">
-        <AvatarFallback>{name.charAt(0)}</AvatarFallback>
-      </Avatar>
-      <h3 className="text-lg font-semibold">{name}</h3>
-      <p className="text-sm text-muted-foreground">@{username}</p>
-      <p className="mt-4">{text}</p>
+    <div
+      className={cn(
+        "p-6 border rounded-lg flex flex-col text-start shadow-glow-lg bg-accent/10",
+        className
+      )}
+    >
+      <div className="flex gap-3 items-center">
+        <Avatar className="w-12 h-12">
+          <AvatarFallback>{name.charAt(0)}</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col items-start">
+          <h3 className="text-lg font-semibold leading-none">{name}</h3>
+          <p className="text-sm text-muted-foreground">@{username}</p>
+        </div>
+      </div>
+      <p className="mt-4 text-md text-muted-foreground">{text}</p>
     </div>
   );
 }
