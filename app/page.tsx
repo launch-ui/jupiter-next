f1ff46de462dd912386ab14d53bc1ae7a370c754
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2024-10-08 16:35:19 +0200

    Fix paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@/components/sections/navbar";
-import Hero from "@/components/sections/hero";
-import Logos from "@/components/sections/logos";
-import Items from "@/components/sections/items";
-import FAQ from "@/components/sections/faq";
-import CTA from "@/components/sections/cta";
-import Footer from "@/components/sections/footer";
+import Navbar from "../components/sections/navbar";
+import Hero from "../components/sections/hero";
+import Logos from "../components/sections/logos";
+import Items from "../components/sections/items";
+import FAQ from "../components/sections/faq";
+import CTA from "../components/sections/cta";
+import Footer from "../components/sections/footer";
 
 export default function Home() {
   return (
