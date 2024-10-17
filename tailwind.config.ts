7b17d41e87650f35b251def25c828381fe61798a
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2024-10-17 11:04:01 +0200

    Make xl border radius dynamic for theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -56,6 +56,7 @@ const config = {
         },
       },
       borderRadius: {
+        xl: "calc(var(--radius) + 2px)",
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
