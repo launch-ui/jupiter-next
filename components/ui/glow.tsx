bb693207bd1e6e0fb7a75cdfc26cfda8ed0fafa9
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2024-10-29 12:07:17 +0100

    Add more glow variants

--- components/ui/glow.tsx
+++ components/ui/glow.tsx
@@ -7,7 +7,9 @@ const glowVariants = cva("absolute w-full opacity-0", {
     variant: {
       top: "top-0",
       above: "-top-[128px]",
-      bottom: "-bottom-[128px]",
+      bottom: "bottom-0",
+      below: "-bottom-[128px]",
+      center: "top-[50%]",
     },
   },
   defaultVariants: {
@@ -28,8 +30,18 @@ const Glow = React.forwardRef<
     className={cn(glowVariants({ variant }), className)}
     {...props}
   >
-    <div className="absolute left-1/2 h-[256px] w-[80%] -translate-x-1/2 rounded-[50%] bg-brand-foreground blur-[256px]" />
-    <div className="absolute left-1/2 top-[32px] h-[128px] w-[60%] -translate-x-1/2 rounded-[50%] bg-brand blur-[64px]" />
+    <div
+      className={cn(
+        "absolute left-1/2 h-[256px] w-[80%] -translate-x-1/2 rounded-[50%] bg-brand-foreground blur-[256px]",
+        variant === "center" && "h-[512px] w-[100%] -translate-y-1/2",
+      )}
+    />
+    <div
+      className={cn(
+        "absolute left-1/2 top-[32px] h-[128px] w-[60%] -translate-x-1/2 rounded-[50%] bg-brand blur-[64px]",
+        variant === "center" && "h-[256px] w-[80%] -translate-y-1/2",
+      )}
+    />
   </div>
 ));
 Glow.displayName = "Glow";
