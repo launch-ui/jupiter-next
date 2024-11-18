c85b35377b654e233061c34a19487163a4881e2b
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2024-11-18 11:48:57 +0100

    Change bg of primary buttons to gradient

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "text-primary-foreground shadow hover:from-primary/80 hover:to-primary/70 bg-gradient-to-b from-primary/90 to-primary/80 border-t-primary",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
