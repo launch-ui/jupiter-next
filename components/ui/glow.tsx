2659a7b79f59c9431f79b575c1883bb53aa37d38
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2024-11-18 20:19:04 +0100

    Adjust glow component for mobile

--- components/ui/glow.tsx
+++ components/ui/glow.tsx
@@ -28,13 +28,13 @@ const Glow = React.forwardRef<
   >
     <div
       className={cn(
-        "absolute left-1/2 h-[512px] w-[60%] -translate-x-1/2 scale-[2.5] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand-foreground)/.5)_10%,_hsla(var(--brand-foreground)/0)_60%)]",
+        "absolute left-1/2 h-[256px] w-[60%] -translate-x-1/2 scale-[2.5] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand-foreground)/.5)_10%,_hsla(var(--brand-foreground)/0)_60%)] sm:h-[512px]",
         variant === "center" && "-translate-y-1/2",
       )}
     />
     <div
       className={cn(
-        "absolute left-1/2 h-[256px] w-[40%] -translate-x-1/2 scale-[2] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand)/.3)_10%,_hsla(var(--brand-foreground)/0)_60%)]",
+        "absolute left-1/2 h-[128px] w-[40%] -translate-x-1/2 scale-[2] rounded-[50%] bg-[radial-gradient(ellipse_at_center,_hsla(var(--brand)/.3)_10%,_hsla(var(--brand-foreground)/0)_60%)] sm:h-[256px]",
         variant === "center" && "-translate-y-1/2",
       )}
     />
